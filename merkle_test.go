8cb3c8d002fca20e352e75e49f1c556274e0c840
AsterNighT <asternight@outlook.com>
2023-03-21 11:54:11 +0800

    Add bench

--- merkle_test.go
+++ merkle_test.go
@@ -5,75 +5,96 @@ package main
 
 import (
 	"encoding/binary"
-	"fmt"
 	"io"
 	"math"
 	"testing"
 
 	"github.com/si-co/vpir-code/lib/client"
 	"github.com/si-co/vpir-code/lib/database"
-	"github.com/si-co/vpir-code/lib/field"
-	"github.com/si-co/vpir-code/lib/monitor"
 	"github.com/si-co/vpir-code/lib/server"
 	"github.com/si-co/vpir-code/lib/utils"
-	"github.com/stretchr/testify/require"
 )
 
-func TestMerkle(t *testing.T) {
+const (
+	oneB            = 8
+	oneKB           = 1024 * oneB
+	oneMB           = 1024 * oneKB
+	oneGB           = 1024 * oneMB
+	testBlockLength = 16
+)
+
+var randomDB *database.DB
+
+func BenchmarkMerkle(b *testing.B) {
 	numServers := 2
-	dbLen := oneMB
-	blockLen := testBlockLength * field.Bytes
+	dbLen := oneMB * 128
+	blockLen := testBlockLength
 	// since this scheme works on bytes, the bit size of one element is 8
 	elemBitSize := 8
 	numBlocks := dbLen / (elemBitSize * blockLen)
 	nCols := int(math.Sqrt(float64(numBlocks)))
-	nRows := nCols
+	nRows := numBlocks / nCols
 
 	db := database.CreateRandomMerkle(utils.RandomPRG(), dbLen, nRows, blockLen)
 
-	retrieveBlocksMerkle(t, utils.RandomPRG(), db, numServers, numBlocks, "Merkle")
+	retrieveBlocksMerkle(b, utils.RandomPRG(), db, numServers, numBlocks, "Merkle")
 }
 
-func TestMerkleFourServers(t *testing.T) {
-	numServers := 4
-	dbLen := oneMB
-	blockLen := testBlockLength * field.Bytes
-	// since this scheme works on bytes, the bit size of one element is 8
-	elemBitSize := 8
-	numBlocks := dbLen / (elemBitSize * blockLen)
-	nCols := int(math.Sqrt(float64(numBlocks)))
-	nRows := nCols
-
-	db := database.CreateRandomMerkle(utils.RandomPRG(), dbLen, nRows, blockLen)
-
-	retrieveBlocksMerkle(t, utils.RandomPRG(), db, numServers, numBlocks, "MerkleFourServers")
-}
-
-func retrieveBlocksMerkle(t *testing.T, rnd io.Reader, db *database.Bytes, numServers, numBlocks int, testName string) {
+func retrieveBlocksMerkle(b *testing.B, rnd io.Reader, db *database.Bytes, numServers, numBlocks int, testName string) {
 	c := client.NewPIR(rnd, &db.Info)
 	servers := make([]*server.PIR, numServers)
 	for i := range servers {
 		servers[i] = server.NewPIR(db)
 	}
-
-	totalTimer := monitor.NewMonitor()
-	for i := 0; i < numBlocks; i++ {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
 		in := make([]byte, 4)
-		binary.BigEndian.PutUint32(in, uint32(i))
-		queries, err := c.QueryBytes(in, numServers)
-		require.NoError(t, err)
+		binary.BigEndian.PutUint32(in, uint32(0))
+		queries, _ := c.QueryBytes(in, numServers)
 
 		answers := make([][]byte, numServers)
 		for i, s := range servers {
-			a, err := s.AnswerBytes(queries[i])
-			require.NoError(t, err)
-			answers[i] = a
+			s.AnswerBytes(queries[i])
 		}
 
-		res, err := c.ReconstructBytes(answers)
-		require.NoError(t, err)
-		require.Equal(t, db.Entries[i*db.BlockSize:(i+1)*db.BlockSize-db.ProofLen-1], res)
+		c.ReconstructBytes(answers)
 	}
+}
 
-	fmt.Printf("Total CPU time %s: %.1fms\n", testName, totalTimer.Record())
+// Test suite for classical PIR, used as baseline for the experiments.
+
+func BenchmarkPIRPoint(b *testing.B) {
+	dbLen := oneMB
+	blockLen := testBlockLength
+	elemBitSize := 8
+	numBlocks := dbLen / (elemBitSize * blockLen)
+	nCols := int(math.Sqrt(float64(numBlocks)))
+	nRows := numBlocks / nCols
+
+	// functions defined in vpir_test.go
+	xofDB := utils.RandomPRG()
+	xof := utils.RandomPRG()
+
+	db := database.CreateRandomBytes(xofDB, dbLen, nRows, blockLen)
+
+	retrievePIRPoint(b, xof, db, numBlocks, "PIRPoint")
+}
+
+func retrievePIRPoint(b *testing.B, rnd io.Reader, db *database.Bytes, numBlocks int, testName string) {
+	c := client.NewPIR(rnd, &db.Info)
+	s0 := server.NewPIR(db)
+	s1 := server.NewPIR(db)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		in := make([]byte, 4)
+		binary.BigEndian.PutUint32(in, uint32(0))
+		queries, _ := c.QueryBytes(in, 2)
+
+		a0, _ := s0.AnswerBytes(queries[0])
+		a1, _ := s1.AnswerBytes(queries[1])
+
+		answers := [][]byte{a0, a1}
+
+		c.ReconstructBytes(answers)
+	}
 }
